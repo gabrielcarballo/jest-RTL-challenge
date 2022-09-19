501c3dfc61a4b03dc51a8291c798db7af1437ad9
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-09-19 18:45:42 -0300

    First part of 1st test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Requisite 1 tests', () => {
+  test('Testing if Home <link> is renderized ', () => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+    const testTarget = screen.getByRole('link', { name: 'Home' });
+    expect(testTarget).toBeInTheDocument();
+  });
+
+  test('Testing if About <link> is renderized ', () => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+    expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+  });
+
+  test('Testing if Favorite Pokémons <link> is renderized', () => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+    expect(screen.getByRole('link', { name: 'Favorite Pokémons' })).toBeInTheDocument();
+  });
+});
