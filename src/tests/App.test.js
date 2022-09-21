c102a4455dfcdb016fd6b57b8796a4151b245b44
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-09-20 22:01:04 -0300

    Learning why testing elements on page instead of URL

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+
 import App from '../App';
 
-describe('Requisite 1 tests', () => {
-  test('Testing if Home <link> is renderized ', () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
+const renderApp = () => renderWithRouter(<App />);
+
+describe('Requisite 1 <link> tests', () => {
+  test('Testing if Home <link> is renderized and correct text', () => {
+    renderApp();
     const testTarget = screen.getByRole('link', { name: 'Home' });
     expect(testTarget).toBeInTheDocument();
+    expect(testTarget).toHaveTextContent('Home');
   });
 
-  test('Testing if About <link> is renderized ', () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
-    expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+  test('Testing if About <link> is renderized and correct text', () => {
+    renderApp();
+    const testTarget = screen.getByRole('link', { name: 'About' });
+    expect(testTarget).toBeInTheDocument();
+    expect(testTarget).toHaveTextContent('About');
   });
 
-  test('Testing if Favorite Pokémons <link> is renderized', () => {
-    render(<BrowserRouter><App /></BrowserRouter>);
-    expect(screen.getByRole('link', { name: 'Favorite Pokémons' })).toBeInTheDocument();
+  test('Testing if Favorite Pokémons <link> is renderized and correct text', () => {
+    renderApp();
+    const testTarget = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(testTarget).toBeInTheDocument();
+    expect(testTarget).toHaveTextContent('Favorite Pokémons');
+  });
+});
+
+describe('Requisite 1 Routing tests', () => {
+  test('Testing if Home link redirects to "/" route', () => {
+    renderApp();
+    const testTarget = screen.getByRole('link', { name: /home/i });
+    userEvent.click(testTarget);
+    expect(screen.getByRole('heading', { level: 1, name: /pokédex/i }))
+      .toBeInTheDocument();
   });
 });
