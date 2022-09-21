cee11ea69374abe927822b9f7d435abce20a3101
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-09-20 23:01:19 -0300

    Requisite 1 done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 
@@ -36,6 +36,32 @@ describe('Requisite 1 Routing tests', () => {
     const testTarget = screen.getByRole('link', { name: /home/i });
     userEvent.click(testTarget);
     expect(screen.getByRole('heading', { level: 1, name: /pokédex/i }))
+      .toBeInTheDocument();
+  });
+
+  test('Testing if About link redirects to "/about" route', () => {
+    renderApp();
+    const testTarget = screen.getByRole('link', { name: /about/i });
+    userEvent.click(testTarget);
+    expect(screen.getByRole('heading', { level: 2, name: /about pokédex/i }))
+      .toBeInTheDocument();
+  });
+
+  test('Testing if Pokémons Favoritados link redirects to "/favorites" route', () => {
+    renderApp();
+    const testTarget = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(testTarget);
+    expect(screen.getByRole('heading', { level: 2, name: /favorite pokémons/i }))
+      .toBeInTheDocument();
+  });
+
+  test('Testing if unrecognized route redirects to "/Not Found" route', () => {
+    const { history } = renderApp();
+    act(() => {
+      history.push('/test');
+    });
+
+    expect(screen.getByRole('heading', { level: 2, name: /page requested not found/i }))
       .toBeInTheDocument();
   });
 });
